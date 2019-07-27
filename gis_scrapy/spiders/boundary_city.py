c60c19bfc0156cd39ca44eb1787f607433961359
YangWanjun <ywjsailor@gmail.com>
2019-07-28 00:52:19 +0900

    scrap cities

--- gis_scrapy/spiders/boundary_city.py
+++ gis_scrapy/spiders/boundary_city.py
@@ -1,27 +1,63 @@
-import scrapy
 import zipfile
 import os
+import shutil
+import psycopg2
+import json
+import datetime
 from io import BytesIO
 from gis_scrapy.utils import common
+from gis_scrapy.utils.base_scrapy import BaseSpider
 
 # 参照ＵＲＬ
 # https://www.esrij.com/products/japan-shp/
+TEMP_DIR = os.path.join(common.get_storage_path(), 'Boundary', 'city')
 
 
-class BoundaryCitySpider(scrapy.Spider):
+class BoundaryCitySpider(BaseSpider):
     name = 'boundary_city'
-    custom_settings = {
-        'LOG_LEVEL': 'ERROR',
-    }
 
     start_urls = [
         'https://www.esrij.com/cgi-bin/wp/wp-content/uploads/2017/01/japan_ver81.zip',
     ]
 
     def parse(self, response):
-        path = os.path.join(common.get_storage_path(), 'Boundary', 'city')
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
+        if os.path.exists(TEMP_DIR):
+            shutil.rmtree(TEMP_DIR)
+        os.makedirs(TEMP_DIR, exist_ok=True)
         zip_ref = zipfile.ZipFile(BytesIO(response.body))
-        zip_ref.extractall(path)
+        zip_ref.extractall(TEMP_DIR)
         zip_ref.close()
+        try:
+            self.save_to_database()
+        finally:
+            shutil.rmtree(TEMP_DIR)
+
+    def save_to_database(self):
+        conn_string = self.settings.get('POSTGRESQL_URL')
+        with psycopg2.connect(conn_string) as conn:
+            with conn.cursor() as cursor:
+                for filename in os.listdir(TEMP_DIR):
+                    if not filename.endswith('.shp'):
+                        continue
+                    for data in common.read_shape_file(os.path.join(TEMP_DIR, filename), encoding='cp932', srid=4326):
+                        sql = "INSERT INTO gis_city (" \
+                              "    pref_code, pref_name, " \
+                              "    city_code, city_name, city_name_en, gun_name, " \
+                              "    people_count, family_count, mpoly, " \
+                              "    created_dt, updated_dt, is_deleted" \
+                              ") " \
+                              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, ST_Multi(ST_GeomFromGeoJSON(%s)), %s, %s, %s);"
+                        cursor.execute(sql, (
+                            data.get('JCODE')[:2],
+                            data.get('KEN'),
+                            data.get('JCODE'),
+                            data.get('SIKUCHOSON'),
+                            data.get('CITY_ENG'),
+                            data.get('GUN'),
+                            data.get('P_NUM'),
+                            data.get('H_NUM'),
+                            json.dumps(data.get('geom')),
+                            datetime.datetime.now(),
+                            datetime.datetime.now(),
+                            False,
+                        ))
--- gis_scrapy/utils/common.py
+++ gis_scrapy/utils/common.py
@@ -1,4 +1,5 @@
 import os
+import shapefile
 
 
 def get_storage_path():
@@ -7,3 +8,17 @@ def get_storage_path():
     if not os.path.exists(path):
         os.mkdir(path)
     return path
+
+
+def read_shape_file(path, encoding='cp932', srid=4326):
+    if not os.path.exists(path):
+        raise Exception('シェープファイルが存在しません。')
+    shp = shapefile.Reader(path, encoding=encoding)
+    fields = shp.fields[1:]
+    for shape_record in shp.iterShapeRecords():
+        data = {}
+        for f_name, f_type, f_length, f_decmail_length in fields:
+            data[f_name] = shape_record.record[f_name]
+        data['geom'] = shape_record.shape.__geo_interface__
+        data['geom']['crs'] = {"type": "name", "properties": {"name": "EPSG:{}".format(srid)}}
+        yield data
