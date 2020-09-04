05022c64c23cb36855fefff22f1a7859c2b821a6
Yang Wanjun <ywjsailor@gmail.com>
2020-09-04 10:41:38 +0900

    city code

--- gis_scrapy/utils/common.py
+++ gis_scrapy/utils/common.py
@@ -34,9 +34,38 @@ def get_pref_code_by_name(name):
 
 
 def get_city_code_by_name(cursor, pref_name, city_name):
-    cursor.execute(
-        "SELECT city_code FROM gis_city WHERE pref_name = %s AND city_name = %s;",
-        (pref_name, city_name)
-    )
-    ret_value = cursor.fetchone()
-    return ret_value[0] if ret_value else None
+    if len(city_name[1:].split('郡')) > 1 and city_name[-1] != '郡' and city_name[-2:] not in ('郡市', '郡村') and \
+            city_name not in ('大和郡山市',):
+        index = city_name[1:].find('郡') + 1
+        cursor.execute(
+            "SELECT city_code FROM gis_city WHERE pref_name = %s AND gun_name = %s AND city_name = %s;",
+            (pref_name, city_name[:index + 1], city_name[index + 1:])
+        )
+        ret_value = cursor.fetchone()
+    elif len(city_name[1:].split('市')) > 1 and city_name[-1] != '市' and city_name[-2:] not in ('市郡', '市村'):
+        index = city_name[1:].find('市') + 1
+        cursor.execute(
+            "SELECT city_code FROM gis_city WHERE pref_name = %s AND city_name = %s;",
+            (pref_name, city_name[index + 1:])
+        )
+        ret_value = cursor.fetchone()
+    elif len(city_name[1:].split('島')) > 1 and city_name[-2:] not in ('島郡', '島市', '島村', '島区', '島町') and \
+            city_name not in ('南島原市',):
+        index = city_name[1:].find('島') + 1
+        cursor.execute(
+            "SELECT city_code FROM gis_city WHERE pref_name = %s AND city_name = %s;",
+            (pref_name, city_name[index + 1:])
+        )
+        ret_value = cursor.fetchone()
+    else:
+        if city_name in ('那珂川市', '那珂川町'):
+            city_name = '那珂川町'
+        cursor.execute(
+            "SELECT city_code FROM gis_city WHERE pref_name = %s AND city_name = %s;",
+            (pref_name, city_name)
+        )
+        ret_value = cursor.fetchone()
+    if ret_value:
+        return ret_value[0]
+    else:
+        return None
